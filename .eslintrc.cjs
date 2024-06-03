8658eac515a9e729dae2f78a5f13eb740d3857d0
greguretzky <uretsky.zvi@gmail.com>
2024-06-03 21:59:36 +0200

    chore: Disable specific TypeScript ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,12 @@ const config = {
   ],
   "rules": {
     "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
